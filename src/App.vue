e504300ca3b4e13016a070d469ae6845cf803b09
tung1998 <tung16021220@gmail.com>
2019-12-11 01:58:36 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    adasdsa
     <router-view/>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import LoginPage from '@/components/LoginPage/LoginPage.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  mode: 'history',
+  routes: [{
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'index',
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginPage,
+      beforeEnter: beforeEnterLogin
     }
   ]
 })
+
+//suport function
+function beforeEnterLogin(to, from, next) {
+  console.log(1)
+  // let userID = Meteor.userId()
+  // if (userID) {
+  //   meteorCallDefaul("user.getByID", userID)
+  //     .then(user => {
+  //       next({
+  //         name: 'user',
+  //         params: {
+  //           user: user.username
+  //         }
+  //       })
+  //     })
+  //     .catch(error => {
+  //       next('user')
+  //     })
+  // } else next()
+}
