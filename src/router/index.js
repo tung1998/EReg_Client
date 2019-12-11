67d556c8312e714f779e9fe37766bf01fe078446
tung1998 <tung16021220@gmail.com>
2019-12-11 16:19:19 +0700

    update route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
+//init vue
 import Vue from 'vue'
 import Router from 'vue-router'
+//import page
 import LoginPage from '@/components/LoginPage/LoginPage.vue'
 import ErrorPage from '@/components/ErrorPage/ErrorPage.vue'
+//import manager component
 import ManagerWorkingPage from '@/components/Manager/WorkingPage/WorkingPage.vue'
-import StudentWorkingPage from '@/components/Student/WorkingPage/WorkingPage.vue'
 import ManagerProfile from '@/components/Manager/Profile/Profile.vue'
+import SubjectManage from '@/components/Manager/SubjectManage/SubjectManage.vue'
+//import student component
+import StudentWorkingPage from '@/components/Student/WorkingPage/WorkingPage.vue'
 import StudentProfile from '@/components/Student/Profile/Profile.vue'
+
+//manager
 
 Vue.use(Router)
 
@@ -26,30 +33,30 @@ export default new Router({
       path: '/manager',
       component: ManagerWorkingPage,
       children: [{
-          name: 'manager',
-          path: '',
-          redirect: 'profile'
-        },
-        {
-          name: 'managerProfile',
-          path: 'profile',
-          component: ManagerProfile
-        }
-      ]
+        name: 'manager',
+        path: '',
+        redirect: 'profile'
+      }, {
+        name: 'managerProfile',
+        path: 'profile',
+        component: ManagerProfile
+      }, {
+        name: 'subjectManage',
+        path: 'profile',
+        component: SubjectManage
+      }]
     }, {
       path: '/student',
       component: StudentWorkingPage,
       children: [{
-          name: 'student',
-          path: '',
-          redirect: 'profile'
-        },
-        {
-          name: 'profile',
-          path: 'profile',
-          component: StudentProfile
-        }
-      ]
+        name: 'student',
+        path: '',
+        redirect: 'profile'
+      }, {
+        name: 'profile',
+        path: 'profile',
+        component: StudentProfile
+      }]
     },
     {
       name: "error",
