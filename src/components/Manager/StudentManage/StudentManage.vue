b12d476c283313ac19433b65c81171dc5a3fd97c
tung1998 <tung16021220@gmail.com>
2019-12-17 18:41:36 +0700

    init studentmanage

--- src/components/Manager/StudentManage/StudentManage.vue
+++ src/components/Manager/StudentManage/StudentManage.vue
@@ -1,12 +1,41 @@
-<template src='./StudentManage.html'></template>
+<template src='./RoomManage.html'></template>
 
 <!--<script src="./Login.js"></script>-->
 <script>
+import AddRoomModal from "./AddRoom/AddRoom.vue";
+import TableRoom from "./TableRoom/Table";
+import { getAllRoom } from "../../../api/room";
+import { handleError } from "../../../helper/function";
+
+let accessToken;
+
 export default {
-  name: "WorkingPage",
+  name: "RoomManage",
+  components: { AddRoomModal, TableRoom },
+  data() {
+    return {
+      RoomList: [],
+      onEditing: false
+    };
+  },
+  methods: {
+    addRoom
+  },
+  created
 };
 
-
+//methods
+function addRoom(data) {
+  this.RoomList.push(data);
+}
+//cycleHook
+function created() {
+  getAllRoom(this.$cookies.get("accessToken"))
+    .then(result => {
+      this.RoomList = result.data;
+    })
+    .catch(handleError);
+}
 </script>
 
 <style scoped>
