9e26c0dacc565b57a3df720f64a611e6afb70af6
tung1998 <tung16021220@gmail.com>
2019-12-17 18:41:42 +0700

    init studentmanage

---
+++ src/components/Manager/StudentManage/TableStudent/Table.vue
@@ -0,0 +1,32 @@
+<template src="./Table.html"></template>
+
+<script>
+import TableRow from "./Row.vue";
+
+export default {
+  name: "ModuleManagerTable",
+  components: {
+    TableRow
+  },
+  props: {
+    RoomList: Array
+  },
+  data() {
+    return {
+      Fields: ["STT", "Phòng", "Địa chỉ", "Số máy tính", ""],
+    };
+  },
+  methods:{
+    deleteRoom
+  }
+};
+
+//methods
+function deleteRoom(index) {
+  this.RoomList.splice(index, 1);
+}
+//support function
+</script>
+
+<style>
+</style>
